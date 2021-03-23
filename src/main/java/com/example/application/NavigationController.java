5264ce7238e54f35d436cda6070c5104c6b0ca2c
andreas-repo <andreas.mayer@gepardec.com>
2021-03-23 22:26:18 +0100

    Added NavigationController Added navigation via automatic implicit navigation

---
+++ src/main/java/com/example/application/NavigationController.java
@@ -0,0 +1,17 @@
+package com.example.application;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.io.Serializable;
+
+@ManagedBean(name = "navigationController", eager = true)
+@RequestScoped
+public class NavigationController implements Serializable {
+    public String moveToPageTwo() {
+        return "page2";
+    }
+
+    public String moveToLandingPage() {
+        return "index";
+    }
+}
