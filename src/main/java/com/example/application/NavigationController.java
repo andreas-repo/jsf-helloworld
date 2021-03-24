77ec012e6c2def3126e22c2c8461f9a9a375393e
andreas-repo <andreas.mayer@gepardec.com>
2021-03-24 20:04:19 +0100

    - fixed conditional navigation

--- src/main/java/com/example/application/NavigationController.java
+++ src/main/java/com/example/application/NavigationController.java
@@ -8,6 +8,13 @@ import java.io.Serializable;
 @ManagedBean(name = "navigationController", eager = true)
 @RequestScoped
 public class NavigationController implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @ManagedProperty(value = "#{param.pageId}")
+    private String pageId;
+
+    public String moveToPageOne() {
+        return "page1";
+    }
 
     public String moveToPageTwo() {
         return "page2";
@@ -15,5 +22,27 @@ public class NavigationController implements Serializable {
 
     public String moveToLandingPage() {
         return "index";
+    }
+
+    public String showPage() {
+        if(pageId == null) {
+            return "index";
+        }
+
+        if(pageId.equals("1")) {
+            return "page1";
+        }else if(pageId.equals("2")) {
+            return "page2";
+        }else {
+            return "index";
+        }
+    }
+
+    public String getPageId() {
+        return pageId;
+    }
+
+    public void setPageId(String pageId) {
+        this.pageId = pageId;
     }
 }
