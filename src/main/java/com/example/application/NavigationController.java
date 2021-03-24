57a560cec43b1919d7249798bb135a9250091ffb
andreas-repo <andreas.mayer@gepardec.com>
2021-03-24 20:07:47 +0100

    - added conditional navigation

--- src/main/java/com/example/application/NavigationController.java
+++ src/main/java/com/example/application/NavigationController.java
@@ -1,6 +1,7 @@
 package com.example.application;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import java.io.Serializable;
 
@@ -8,11 +9,40 @@ import java.io.Serializable;
 @RequestScoped
 public class NavigationController implements Serializable {
     private static final long serialVersionUID = 1L;
+    @ManagedProperty(value = "#{param.pageId}")
+    private String pageId;
+
+    public String moveToPageOne() {
+        return "page1";
+    }
+
     public String moveToPageTwo() {
         return "page2";
     }
 
     public String moveToLandingPage() {
         return "index";
+    }
+
+    public String showPage() {
+        if(pageId == null) {
+            return "index";
+        }
+
+        if(pageId.equals("1")) {
+            return "page1";
+        }else if(pageId.equals("2")) {
+            return "page2";
+        }else {
+            return "index";
+        }
+    }
+
+    public String getPageId() {
+        return pageId;
+    }
+
+    public void setPageId(String pageId) {
+        this.pageId = pageId;
     }
 }
